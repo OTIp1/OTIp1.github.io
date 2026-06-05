215fb230d45847d08cc7d9f87c91ae5f7fa34c55
Root <root.rosenberg@gmail.com>
2026-06-05 15:55:41 -0400

    didn't do anything

---
+++ defineactive.js
@@ -0,0 +1,19 @@
+/*const currentPath = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll('.nav ul li a');
+
+function funFunction(){
+    var page_name = window.location.pathname.split('/').pop()
+    ('a[href$="'+ page_name +'"]').parent().addClass('active');
+}
+
+
+navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href').split("/").pop();
+    if (currentPath == linkPath) {
+    link.parentElement.classList.add('active');
+    }
+    else {
+    link.parentElement.classList.remove('active');
+    }
+});
+*/
