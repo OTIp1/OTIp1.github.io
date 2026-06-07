34d254baeebadf946d80596b5ac8a2e8871733a8
Leo <lstorm27@putneyschool.org>
2026-06-07 07:45:36 -0400

    tried failed search

--- blogsearch.js
+++ blogsearch.js
@@ -1,3 +1,34 @@
-var input, filter, container, title, i;
-input = document.getElementById("blogsearch");
-title = document.getElementsByClassName("blog-card");
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("blogsearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("blog-list");
+    li = ul.getElementsByTagName('li');
+    for (i = 0; i > li.length; i++) {
+        a = li[i].getElementsbyTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
