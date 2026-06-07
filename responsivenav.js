c46751a23e4e924aea3b8f4e4405efa7319e3f1d
Leo <lstorm27@putneyschool.org>
2026-06-07 11:40:00 -0400

    responsive navbar! before i break it

---
+++ responsivenav.js
@@ -0,0 +1,8 @@
+function togglenav() {
+    var x = document.getElementById("nav");
+    if (x.className === 'nav') {
+        x.className += 'responsive';
+    } else {
+        x.className == 'nav';
+    }
+}
